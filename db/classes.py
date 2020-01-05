48ca8d10d7f496759ae1f9789b571d1955fabbdc
Artem Bakhanov <artembakhanov@gmail.com>
2020-01-06 02:26:54 +0300

    Change database url

--- db/classes.py
+++ db/classes.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(os.environ["DB"])
+engine = create_engine(os.environ["DATABASE_URL"])
 base = declarative_base()
 
 
