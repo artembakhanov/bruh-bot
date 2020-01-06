cbf4f321cf18ce8b93fbaefa82b1f12a4161c7eb
Artem Bakhanov <artembakhanov@gmail.com>
2020-01-06 20:12:51 +0300

    Change static params

--- bot/static.py
+++ bot/static.py
@@ -11,13 +11,11 @@ def START_MESSAGE(user, not_registered):
 
 DNE_MESSAGE = f"Sorry, but you are not in our databases. Please, type /start"
 AUDION_MESSAGE = f"Send me your bruh audio!"
-RECORDED_MESSAGE = f"Your bruh has been successfully recorded!\n" \
-                   f"We will verify it and then everyone will be able to use it.\n" \
-                   f"Thanks!"
+RECORDED_MESSAGE = f"Your bruh has been recorded. It will be made (become) public after verification."
 
 commands = ["bruh", "bruh\U0001F50A", "Record my bruh\U0001F3A4"]
 
-COMMANDS_KEYBOARD = types.ReplyKeyboardMarkup(row_width=2)
+COMMANDS_KEYBOARD = types.ReplyKeyboardMarkup(row_width=2, selective=True)
 COMMANDS_KEYBOARD.add(*commands)
 
 HIDE_KEYBOARD = types.ReplyKeyboardRemove()
