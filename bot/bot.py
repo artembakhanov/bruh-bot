5e503b97ba40b045ee27a750105cbec7fb9a095c
Artem Bakhanov <artembakhanov@gmail.com>
2020-01-06 13:24:43 +0300

    Add keyboard

--- bot/bot.py
+++ bot/bot.py
@@ -2,7 +2,7 @@ import os
 
 import telebot
 
-from bot.static import START_MESSAGE
+from bot.static import *
 from db.core import create_user
 
 bot = telebot.TeleBot(os.environ["t_token"])
@@ -11,4 +11,4 @@ bot = telebot.TeleBot(os.environ["t_token"])
 @bot.message_handler(commands=['start'])
 @create_user
 def start_message(not_registered, m):
-    bot.send_message(m.chat.id, START_MESSAGE(m.from_user, not_registered))
+    bot.send_message(m.chat.id, START_MESSAGE(m.from_user, not_registered), COMMANDS_KEYBOARD)
