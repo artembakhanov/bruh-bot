ff035008f1fd5d4d1801c33ff4050ce35eecf587
Artem Bakhanov <artembakhanov@gmail.com>
2020-01-06 13:24:01 +0300

    Change start message

--- bot/bot.py
+++ bot/bot.py
@@ -1,28 +1,14 @@
 import os
-from time import sleep
 
 import telebot
 
+from bot.static import START_MESSAGE
 from db.core import create_user
 
 bot = telebot.TeleBot(os.environ["t_token"])
 
 
-@bot.message_handler(commands=['hello'])
-def hello_message(m):
-    sleep(5)
-    print("Hello world")
-
-
-@bot.message_handler(commands=['hello1'])
-def hello_message(m):
-    print("Hello world1")
-
-
 @bot.message_handler(commands=['start'])
 @create_user
 def start_message(not_registered, m):
-    if not_registered:
-        bot.send_message(m.chat.id, "Hello! You are new here... Welcome!")
-    else:
-        bot.send_message(m.chat.id, "Hello! You are already in!")
+    bot.send_message(m.chat.id, START_MESSAGE(m.from_user, not_registered))
