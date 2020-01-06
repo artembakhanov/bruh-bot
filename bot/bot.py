16572a6dcec3bc69a7bf65d95644786fe6cde10c
Artem Bakhanov <artembakhanov@gmail.com>
2020-01-06 13:27:48 +0300

    Fix keyboard

--- bot/bot.py
+++ bot/bot.py
@@ -11,4 +11,4 @@ bot = telebot.TeleBot(os.environ["t_token"])
 @bot.message_handler(commands=['start'])
 @create_user
 def start_message(not_registered, m):
-    bot.send_message(m.chat.id, START_MESSAGE(m.from_user, not_registered), COMMANDS_KEYBOARD)
+    bot.send_message(m.chat.id, START_MESSAGE(m.from_user, not_registered), reply_markup=COMMANDS_KEYBOARD)
