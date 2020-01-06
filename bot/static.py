9bb8735ea33898886ee2602e4fe097f5a7df3ce3
Artem Bakhanov <artembakhanov@gmail.com>
2020-01-06 13:23:50 +0300

    Add static.py

---
+++ bot/static.py
@@ -0,0 +1,13 @@
+from telebot import types
+
+
+def START_MESSAGE(user, not_registered):
+    return f"Hello, {user.first_name}! bruh" if not_registered else f"bruh"
+
+
+commands = ["bruh", "bruh\U0001F50A", "Record my bruh\U0001F3A4"]
+
+COMMANDS_KEYBOARD = types.ReplyKeyboardMarkup(row_width=2)
+COMMANDS_KEYBOARD.add(*commands)
+
+HIDE_KEYBOARD = types.ReplyKeyboardRemove()
